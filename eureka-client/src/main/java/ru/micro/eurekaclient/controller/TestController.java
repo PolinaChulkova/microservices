9d9921b6da19abd6648e81ecc085cd49e3af5f5e
Polina Chulkova <shelkova1937@gmail.com>
2022-11-30 15:43:10 +0500

    В проект добавлен Actuator и настройки api-gateway вынесены в gitHub

--- eureka-client/src/main/java/ru/micro/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/micro/eurekaclient/controller/TestController.java
@@ -12,8 +12,11 @@ public class TestController {
     @Value("${eureka.instance.instance-id}")
     private String id;
 
+    @Value("${tmpVar}")
+    private int tmp;
+
     @GetMapping("/test")
     public String test() {
-        return id;
+        return id + " : " + tmp;
     }
 }
